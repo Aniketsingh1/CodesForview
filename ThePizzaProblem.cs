1692b5716ecada9055d1183dd5cbb4ef577f3845
Aniket Singh <singhaniket264@gmail.com>
2019-09-17 12:36:09 +0530

    Picks out top 20 topping along with used count.
    Program output:
    
    Topping = pepperoni, No.OfTimesOrdered = 5406
    Topping = mozzarella cheese, No.OfTimesOrdered = 1262
    Topping = four cheese, No.OfTimesOrdered = 1156
    Topping = bacon, No.OfTimesOrdered = 1050
    Topping = beef, No.OfTimesOrdered = 810
    Topping = sausage, No.OfTimesOrdered = 542
    Topping = italian sausage, No.OfTimesOrdered = 495
    Topping = mushrooms, No.OfTimesOrdered = 326
    Topping = chicken, No.OfTimesOrdered = 288
    Topping = ham, No.OfTimesOrdered = 246
    Topping = black olives, No.OfTimesOrdered = 191
    Topping = pineapple, No.OfTimesOrdered = 162
    Topping = cheddar cheese, No.OfTimesOrdered = 122
    Topping = jalapenos, No.OfTimesOrdered = 118
    Topping = alredo sauce, No.OfTimesOrdered = 110
    Topping = green peppers, No.OfTimesOrdered = 75
    Topping = canadian bacon, No.OfTimesOrdered = 65
    Topping = hot peppers, No.OfTimesOrdered = 50
    Topping = diced tomatoes, No.OfTimesOrdered = 46
    Topping = sliced breaded chicken breast, No.OfTimesOrdered = 45
    Topping = onions, No.OfTimesOrdered = 36

---
+++ ThePizzaProblem.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+
+class CandidateCode
+{
+
+    public static void ManipulateTheString(dynamic param)
+    {
+        int count = 0;
+        Dictionary<string, int> result = new Dictionary<string, int>();
+        List<string> dataList = new List<string>();
+        foreach (var key in param)
+        {
+            var value = key["toppings"][0];
+            dataList.Add(value);// Add the toppings to a list.
+        }
+        var uniqueToopings = new HashSet<string>(dataList);// Pick out the unique combination of toppings to compare.
+        foreach (var topping in uniqueToopings)
+        {
+            foreach (var toppings in dataList)
+            {
+                if (topping == toppings)
+                {
+                    count++;
+                }
+            }
+            result.Add(topping, count);
+            count = 0;
+        }
+        var sortedDict = (from entry in result orderby entry.Value descending select entry).Take(21); //Take top 20 toppings
+        foreach (var mainTooping in sortedDict)
+        {
+            Console.WriteLine("Topping = {0}, No.OfTimesOrdered = {1}", mainTooping.Key, mainTooping.Value);
+        }
+
+        Console.ReadLine();
+
+    }
+
+
+    static void Main(String[] args)
+    {
+        string json = (new WebClient()).DownloadString(" http://files.olo.com/pizzas.json ");
+        JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+        dynamic dobj = jsonSerializer.Deserialize<dynamic>(json);
+        ManipulateTheString(dobj);
+
+    }
+
+}
+
